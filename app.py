b320dfc6b984e40f17281ca1edc0680b2b3ad230
shenzhenj <97188842+shenzhenj@users.noreply.github.com>
2023-04-21 19:25:18 +0000

    Worked on dynamic templates + jsonify feature.

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+PLACES = [{
+  "id": 1,
+  "title": "Khreschatyk",
+  "meaning": "Heart of Kyiv"
+}, {
+  "id": 2,
+  "title": "Kyiv-Pechersk Lavra",
+  "meaning": "Soul of Kyiv"
+}, {
+  "id": 3,
+  "title": "Hidropark",
+  "meaning": "Health of Kyiv"
+}]
 
 
 @app.route("/")
 def hello_kyiv():
-  return render_template("home.html")
+  return render_template("home.html", places=PLACES)
+
+@app.route("/places")
+def list_places():
+  return jsonify(PLACES)
 
 
 if __name__ == "__main__":
